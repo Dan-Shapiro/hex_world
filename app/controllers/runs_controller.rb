c7099fe7318db322c128b6183cfd79c47ceba6ab
Dan Shapiro <ds.dan.shapiro@gmail.com>
2026-01-16 00:54:36 -0500

    Add unlcok flow with adjacency checks and clickable available hexes

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,3 +1,5 @@
+require "set"
+
 class RunsController < ApplicationController
   def new
     # start screen
@@ -9,10 +11,13 @@ class RunsController < ApplicationController
       status: :active,
       turn: 1,
       state: {
-        "resources" => { "essence" => 0, "favor" => 0, "power" => 0 },
+        "resources" => { "essence" => 0, "favor" => 0, "power" => 0, "air_rune" => 0, "mind_rune" => 0 },
         "alignment" => "neutral"
       }
     )
+
+    center = Hex.find_by!(q: 0, r: 0)
+    RunHex.create!(run: run, hex: center, unlocked_at_turn: run.turn)
 
     redirect_to run_path(run)
   end
@@ -20,5 +25,16 @@ class RunsController < ApplicationController
   def show
     @run = Run.find(params[:id])
     @hexes = Hex.order(:q, :r).all
+    @unlocked_hex_ids = @run.run_hexes.pluck(:hex_id).to_set
+  end
+
+  def unlock
+    @run = Run.find(params[:id])
+    hex = Hex.find(params[:hex_id])
+
+    Game::UnlockService.new(run: @run, hex: hex).unlock!
+    redirect_to run_path(@run), notice: "Unlocked #{hex.data['name']}"
+  rescue Game::UnlockService::UnlockError => e
+    redirect_to run_path(@run), alert: e.message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :runs, only: [ :new, :create, :show ]
+  resources :runs, only: [ :new, :create, :show ] do
+    match :unlock, on: :member, via: [ :get, :post ]
+  end
+
   root "runs#new"
 end
